991769e7c552a8bc95d1697f364404460d5db3d4
raheez66 <raheez66@gmail.com>
2022-12-24 17:38:07 +0400

    added placeholder image

--- app/src/main/java/com/app/imagelisting/MainActivity.kt
+++ app/src/main/java/com/app/imagelisting/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.app.imagelisting
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
 import com.app.imagelisting.ui.theme.ImageListingTheme
 
@@ -23,6 +28,9 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background
                 ) {
                     Greeting("Android")
+
+                    Text(text = "ryan")
+                    Image(painter = painterResource(id = R.drawable.placeholder_image), contentDescription = "place holder image" )
                 }
             }
         }
@@ -31,7 +39,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    //Text(text = "Hello $name!")
+
+
+
 }
 
 @Preview(showBackground = true)
